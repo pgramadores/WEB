2e881df22fed31ac6e6669ebde73b8758fd9530c
Juan Mora <raicerk@gmail.com>
2016-06-11 14:38:30 -0400

    Inicio del proyecto

---
+++ pro-gramadores/system/class.constantes.php
@@ -0,0 +1,9 @@
+<?php
+	
+	#Se definen las constantes de conexión
+	define('hostConexion','raicerk.cl');
+	define('userConexion','cra16464_pro-gra');
+	define('passConexion','pro-gra');
+	define('ddbbConexion','cra16464_pro-gramadores');
+
+?>
