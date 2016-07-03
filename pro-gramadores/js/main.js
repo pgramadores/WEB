b3a161ae0d0381aa0542de031a7549ce921c7d58
Juan Mora <raicerk@gmail.com>
2016-07-02 23:18:46 -0400

    Se limpia archivo

--- pro-gramadores/js/main.js
+++ pro-gramadores/js/main.js
@@ -27,14 +27,6 @@ angularRoutingApp.config(function($routeProvider) {
 });
 
 
-
-
-angularRoutingApp.controller('ContactoController', function($scope, $routeParams) {
-	$scope.message = 'Esta es la página de "Contacto", aquí podemos poner un formulario';
-    $scope.Usuario = $routeParams.Usuario;
-    $scope.Nombre = $routeParams.Nombre;
-});
-
 function OtroController($scope, $http) {
 
     $scope.names = [ ];
