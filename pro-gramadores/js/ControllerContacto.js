8c1de35e460e507771390c93ffb658eaf61a8bba
Juan Mora <raicerk@gmail.com>
2016-07-10 01:35:23 -0400

    Se modifica controlador de contacto para el envio de mensajes desde la pagina, ademas se crea archivo php que recibe y envia respuesta de 'mensaje enviado con exito'

--- pro-gramadores/js/ControllerContacto.js
+++ pro-gramadores/js/ControllerContacto.js
@@ -1,5 +1,17 @@
-angularRoutingApp.controller('ContactoController', function($scope, $routeParams) {
+angularRoutingApp.controller('ContactoController', function($scope, $http) {
 	$scope.message = 'Esta es la página de "Contacto", aquí podemos poner un formulario';
-    $scope.Usuario = $routeParams.Usuario;
-    $scope.Nombre = $routeParams.Nombre;
+    $scope.EnviarContacto = function($event) {
+        $http.post('./system/AngularModels/ContactoEnviaMensaje.php', { 
+        	Nombres: $scope.txtNombres,
+        	Correo: $scope.txtCorreo,
+        	Asunto: $scope.txtAsunto,
+        	Mensaje: $scope.txtMensaje
+        })
+        .success(function(data) {
+        		angular.element($event.target).val(data);
+            })
+        .error(function(data) {
+                console.log('Error: ' + data);
+        });
+    } 
 });
