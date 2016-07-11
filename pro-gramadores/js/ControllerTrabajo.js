a5f63327286156c26fb91b7011c115a747f2beb5
Juan Mora <raicerk@gmail.com>
2016-07-10 22:17:00 -0400

    Se crea modal para postulación a trabajos, mas logica de controlador incluyendo el paso de parametros

--- pro-gramadores/js/ControllerTrabajo.js
+++ pro-gramadores/js/ControllerTrabajo.js
@@ -1,12 +1,9 @@
-angularRoutingApp.controller('TrabajoController', function($scope,$http) {
-	$scope.message = 'Esta es la página "Acerca de"';
+angularRoutingApp.controller('TrabajoController', function($scope,$http,$uibModal,$rootScope) {
+	
 	$http.get('./system/AngularModels/TrabajosListar.php')
     .success(function(data) {
-        $scope.contents = data;
 
-        /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-        /* :::::::::::::::::::::::::::::::Sistema de paginacion:::::::::::::::::::::::::::::::::::::::: */
-        /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+        $scope.contents = data;
 
         $scope.makeTodos = function() {
 	    	$scope.todos = [];
@@ -26,10 +23,32 @@ angularRoutingApp.controller('TrabajoController', function($scope,$http) {
 	    	$scope.filteredTodos = $scope.todos.slice(begin, end);
 	  	});
 	  	
-	  	/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-	  	/* :::::::::::::::::::::::::::::Fin sistema de paginacion:::::::::::::::::::::::::::::::::::::::*/
-	  	/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+	  	$scope.animationsEnabled = true;
+
+		$scope.open = function (idttabajo) {
+
+			$scope.IdEmpleoAPostular = idttabajo;
+
+			var modalInstance = $uibModal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'myModalContent.html',
+				controller: 'TrabajoController',
+				scope: $scope
+			});
+
+			$rootScope.Modal = modalInstance;
+		};
+
+		$scope.ok = function () {
+			$rootScope.Modal.close('cancel');
+		};
+
+		$scope.cancel = function () {
+			$rootScope.Modal.dismiss('cancel');
+		};
+
     });
+
     $scope.Postular = function($idTrabajo,$event) {
         $http.post('./system/AngularModels/TrabajosPostular.php', { idTrabajo: $idTrabajo } )
         .success(function(data) {
