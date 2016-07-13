1d6e515945e8804278904e3c254c7e57925656c1
Juan Mora <raicerk@gmail.com>
2016-07-12 22:51:33 -0400

    Cambios en test

--- pro-gramadores/test.php
+++ pro-gramadores/test.php
@@ -1,16 +1,11 @@
 <?php
 
-#include 'system/ModelClass.php';
-#$sql = new DataMySql();
-#$resultado = $sql->query("CALL spRec_Trabajos_ConsultaTrabajosDisponibles();",'',false);
-#echo json_encode($resultado);
-
 
 include 'abstraccionMysql/sistema/class.controlador.php';
 $funciones = new Funciones();
-#$data = array('i', '1');
-$sql = "CALL spRec_Trabajos_ConsultaTrabajosDisponibles()";
-$resultado = $funciones->query($sql, array(), false);
+$data = array('i', '1');
+$sql = "select * from Trabajos where idTrabajos = ?";
+$resultado = $funciones->query($sql, $data, false);
 foreach ($resultado as $campo) {
     echo $campo['idTrabajos'];
     echo $campo['Cargo'];
