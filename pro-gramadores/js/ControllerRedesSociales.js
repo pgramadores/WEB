5795d1afa450ef650a2f0f0cdfda92da6060f10e
Juan Mora <raicerk@gmail.com>
2016-07-16 20:42:26 -0400

    Se cambia url de acceso para contar cantidad de seguidores de datos de facebook

--- pro-gramadores/js/ControllerRedesSociales.js
+++ pro-gramadores/js/ControllerRedesSociales.js
@@ -1,8 +1,8 @@
 angularRoutingApp.controller('RedesSocialesController', function($scope,$http) {
 	
-	$http.get('https://graph.facebook.com/pgramadores/insights/page_fans?access_token=EAACEdEose0cBAFh3cHYBVtDutpQ4UkbuA9gFzwsUxqM5m0IhpO0GnjDXS8CvSlypW38xZATeQS4rvheXjNRX6lUHv7VBozPted1HUZBr31c9C7ZBndqbSYO3lXxUvZBgcDtJ1Om34TZA1d4sbLlYJzz6S2tHceUZCQ1rNAMvUvhgZDZD&since=1469304448&until=1469563648')
+	$http.get('https://api.facebook.com/method/fql.query?format=json&query=SELECT total_count FROM link_stat WHERE url="https://facebook.com/pgramadores"')
     .success(function(data) {
-        $scope.Facebook = data.data[0].values[0].value;
+        $scope.Facebook = data[0].total_count;
     });
 
     $http.jsonp('http://api.twittercounter.com/?apikey=031ad25ad5ec74a4c771524f600ef3c4&output=jsonp&callback=JSON_CALLBACK&username=pgramadores')
