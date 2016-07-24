5940f793c69e896129d150c6e932ac2ab95c8c0e
Juan Mora <raicerk@gmail.com>
2016-07-23 23:42:32 -0400

    Se agrega a funcion de datos la lista de trabajos disponibles por pais

--- pro-gramadores/models/TrabajosListar.php
+++ pro-gramadores/models/TrabajosListar.php
@@ -5,7 +5,8 @@ include './system/models.php';
 //var_dump(Utiles::PaisIP());
 
 $sql = new SQL();
-$resultado = $sql->query("CALL spRec_Trabajos_ConsultaTrabajosDisponibles();",'',false);
+$data = array('s',$_GET['Pais']);
+$resultado = $sql->query("CALL spRec_Trabajos_ConsultaTrabajosDisponibles(?);",$data,false);
 
 echo json_encode($resultado);
 
