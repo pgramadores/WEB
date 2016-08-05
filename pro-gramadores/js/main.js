2a636ef92b23a4c5e66547fecf8b056d86d3e86a
Juan Mora <raicerk@gmail.com>
2016-08-04 22:39:02 -0400

    Se agrega sistema de rutas absolutas eliminando el simbolo # en las url para hacer el sistema y manejo de url mas amigable

--- pro-gramadores/js/main.js
+++ pro-gramadores/js/main.js
@@ -2,7 +2,7 @@
 var angularRoutingApp = angular.module('angularRoutingApp', ['ngRoute','ui.bootstrap','ngAnimate','ngSanitize','textAngular','ngImgCrop']);
 
 // // Configuración de las rutas
-angularRoutingApp.config(function($routeProvider) {
+angularRoutingApp.config(function($routeProvider,$locationProvider) {
 
 	$routeProvider
 		.when('/', {
@@ -32,8 +32,24 @@ angularRoutingApp.config(function($routeProvider) {
         .when('/error',{
             templateUrl : 'views/404.html',
             controller  : 'error' 
-        }) 
-		.otherwise({
-			redirectTo: 'error'
-		});
+        })
+        .otherwise({
+            redirectTo: 'error'
+        });
+
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+
+    //check browser support
+        if(window.history && window.history.pushState){
+            //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+
+         // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+
+         // if you don't wish to set base URL then use this
+         $locationProvider.html5Mode({
+                 enabled: true,
+                 requireBase: false
+          });
+        }
 });
