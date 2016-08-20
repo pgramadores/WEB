a26172310e4f4d84b1d8b23488b5aee801b86bc2
Juan Mora <raicerk@gmail.com>
2016-08-20 02:40:27 -0300

    Se crea funcion de llamado a share de Facebook  a traves de la api de facebook para compartir las publicaciones de manera dinamicas

--- pro-gramadores/js/ControllerDetalleTrabajo.js
+++ pro-gramadores/js/ControllerDetalleTrabajo.js
@@ -10,6 +10,21 @@ angularRoutingApp.controller('DetalleTrabajoController', function($scope, $route
         $scope.Detalle = $routeParams.Oferta;
         $scope.Ubicacion = escape($location.absUrl());
         $rootScope.Titulo = data.Cargo;
+
+
+        $scope.CompartirFacebook = function () {
+            console.log($location.absUrl());
+            FB.ui({
+                method: 'feed',
+                name: data.Cargo,
+                link: $location.absUrl(),
+                caption: 'Pro-Gramadores.org :: El poder del código',
+                description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                picture : 'https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAlYAAAAJDQ0MWNjZWFiLTQwZTYtNDJiZC1hNWY3LTM3MWE5OGM1Mzc5MQ.png',
+            }, function(response){
+
+            });
+        };
     });
 
     $scope.animationsEnabled = true;
