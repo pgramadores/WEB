9532a1624156b6263b10613f6f5ca43b17a2cbfe
Juan Mora <raicerk@gmail.com>
2016-08-20 02:39:10 -0300

    Se cambio metodo de rescate de los datos de la pagina de facebook de pro-gramadores

--- pro-gramadores/js/ControllerRedesSociales.js
+++ pro-gramadores/js/ControllerRedesSociales.js
@@ -1,8 +1,8 @@
 angularRoutingApp.controller('RedesSocialesController', function($scope,$http) {
 	
-	$http.get('https://api.facebook.com/method/fql.query?format=json&query=SELECT total_count FROM link_stat WHERE url="https://facebook.com/pgramadores"')
+	$http.get('https://graph.facebook.com/v2.7/456860731019994?fields=fan_count&access_token=EAAIXiYjjEvkBAHbAD7B4UCCW5u0Mw1RpQJ5ici5TEtBkp0Y8m3jHMmSnJR4jO7vV4QJ5j4SISjRqetMkdKd7cIsJPXJw6hUxK5WYMDwExYKxm4PjuLJl42lxmFhvESJSkyPipsyRhnwf2mvb36bG4mYToAlyM95EQignugZDZD')
     .success(function(data) {
-        $scope.Facebook = data[0].total_count;
+        $scope.Facebook = data.fan_count;
     });
 
     $http.jsonp('http://api.twittercounter.com/?apikey=031ad25ad5ec74a4c771524f600ef3c4&output=jsonp&callback=JSON_CALLBACK&username=pgramadores')
