1bac685ebc4113354eb9651a87c4ff6ceed11993
Juan Mora <raicerk@gmail.com>
2016-09-18 23:51:43 -0300

    Se agrega libreria para selector de fecha

--- pro-gramadores/js/main.js
+++ pro-gramadores/js/main.js
@@ -1,60 +1,53 @@
 // Creación del módulo
-var angularRoutingApp = angular.module('angularRoutingApp', ['ngRoute','ui.bootstrap','ngAnimate','ngSanitize','textAngular','ngImgCrop','metatags']);
+var angularRoutingApp = angular.module('angularRoutingApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'textAngular', 'ngImgCrop', 'metatags', 'angularjs-datetime-picker']);
 
 // // Configuración de las rutas
-angularRoutingApp.config(function($routeProvider,$locationProvider,MetaTagsProvider) {
+angularRoutingApp.config(function($routeProvider, $locationProvider, MetaTagsProvider) {
 
-	$routeProvider
-		.when('/', {
-			templateUrl	: 'views/index.html',
-			controller 	: 'mainController'
-		})
-		.when('/trabajos', {
-			templateUrl : 'views/trabajos.html',
-			controller 	: 'TrabajoController'
-		})
-		.when('/detalleoferta/:IdOferta?/:Oferta?/', {
-			templateUrl : 'views/detalletrabajo.html',
-			controller 	: 'DetalleTrabajoController'
-		})
-		.when('/creaoferta', {
-			templateUrl : 'views/creaoferta.html',
-			controller 	: 'CreaTrabajoController'
-		})
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/index.html',
+            controller: 'mainController'
+        })
+        .when('/trabajos', {
+            templateUrl: 'views/trabajos.html',
+            controller: 'TrabajoController'
+        })
+        .when('/detalleoferta/:IdOferta?/:Oferta?/', {
+            templateUrl: 'views/detalletrabajo.html',
+            controller: 'DetalleTrabajoController'
+        })
+        .when('/creaoferta', {
+            templateUrl: 'views/creaoferta.html',
+            controller: 'CreaTrabajoController'
+        })
         .when('/contacto', {
-            templateUrl : 'views/contacto.html',
-            controller  : 'ContactoController'
+            templateUrl: 'views/contacto.html',
+            controller: 'ContactoController'
         })
         .when('/foros', {
-			templateUrl : 'views/foros.html',
-			controller 	: 'ForosController'
-		})
-        .when('/error',{
-            templateUrl : 'views/404.html',
-            controller  : 'error' 
+            templateUrl: 'views/foros.html',
+            controller: 'ForosController'
+        })
+        .when('/error', {
+            templateUrl: 'views/404.html',
+            controller: 'error'
         })
         .otherwise({
             redirectTo: 'error'
         });
 
-        // use the HTML5 History API
-        $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 
-        //check browser support
-        if(window.history && window.history.pushState){
-            //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
-
-         // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
-
-         // if you don't wish to set base URL then use this
-         $locationProvider.html5Mode({
-                 enabled: true,
-                 requireBase: false
-          });
-        }
+    if (window.history && window.history.pushState) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    }
 
     MetaTagsProvider
-          .when('/', {
+        .when('/', {
             title: 'Inicio',
             description: 'Cool',
             fb_title: 'My title',
@@ -65,21 +58,21 @@ angularRoutingApp.config(function($routeProvider,$locationProvider,MetaTagsProvi
             fb_image: 'an_image.jpg',
             robots: 'index, follow',
             keywords: 'some cool keywords'
-          })
-          .when('/detalleoferta/:IdOferta?/:Oferta?/',{
+        })
+        .when('/detalleoferta/:IdOferta?/:Oferta?/', {
             title: 'detalle de la oferta',
-            description: function(IdOferta, Oferta){
+            description: function(IdOferta, Oferta) {
                 return 'COOOOOOOL' + IdOferta + " Super " + Oferta;
             },
             robots: 'index, follow',
             keywords: 'some cool keywords'
-          })
-          .otherwise({
+        })
+        .otherwise({
             title: 'otherwise',
             description: 'Another great page'
-          });
+        });
 });
 
-angularRoutingApp.run(function(MetaTags){
+angularRoutingApp.run(function(MetaTags) {
     MetaTags.initialize();
 });
