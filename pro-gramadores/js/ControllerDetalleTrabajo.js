5f8290f3f257ba6c59c7ba5eade77376088e3d8b
Juan Mora <raicerk@gmail.com>
2016-09-20 00:28:20 -0300

    Se cambia logica de postulacion a ofertas de trabajo, mas el diseño de la postulacion

--- pro-gramadores/js/ControllerDetalleTrabajo.js
+++ pro-gramadores/js/ControllerDetalleTrabajo.js
@@ -27,36 +27,10 @@ angularRoutingApp.controller('DetalleTrabajoController', function($scope, $route
         };
     });
 
-    $scope.animationsEnabled = true;
-
-	$scope.open = function (idttabajo) {
-
-		$scope.IdEmpleoAPostular = idttabajo;
-
-		var modalInstance = $uibModal.open({
-			animation: $scope.animationsEnabled,
-			templateUrl: 'myModalContent.html',
-			controller: 'TrabajoController',
-			scope: $scope
-		});
-
-		$rootScope.Modal = modalInstance;
-	};
-
-	$scope.ok = function () {
-		$rootScope.Modal.close('cancel');
-	};
-
-	$scope.cancel = function () {
-		$rootScope.Modal.dismiss('cancel');
-	};
-
-	$scope.Postular = function($event,$data,$IdTrabajo) {
-
-		var str = JSON.stringify($data);
-		var nuevo = str.substring(0,str.length-1) + ',"idTrabajo":"'+$IdTrabajo+'"}';
-		
-        $http.post('./models/TrabajosPostular.php', nuevo )
+	$scope.Postular = function($event,$IdTrabajo) {		
+        $http.post('./models/TrabajosPostular.php',{
+            idTrabajo: $IdTrabajo,
+        })
         .success(function(data) {
         		angular.element($event.target).html('<i class="fa fa-check"></i> ' + data);
             })
