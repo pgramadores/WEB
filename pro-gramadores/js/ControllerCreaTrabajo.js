f48b84cd6b67ef3eeec64a69337b27651a1c3646
Juan Mora <raicerk@gmail.com>
2016-09-26 20:34:56 -0300

    Se cambian formatos para recepcionar la logica de la nueva base de datos

--- pro-gramadores/js/ControllerCreaTrabajo.js
+++ pro-gramadores/js/ControllerCreaTrabajo.js
@@ -1,22 +1,31 @@
 angularRoutingApp.controller('CreaTrabajoController', function($scope, $http, $rootScope) {
-	
+
     $rootScope.Titulo = "Crea Oferta Laboral";
 
+	var date = new Date();
+
+	$http.get('http://ipinfo.io/json')
+	.success(function(datos) {
+		$scope.Pais = datos.country;
+	});
+
     $scope.CreaOferta = function() {
-        $http.post('./models/TrabajosCrear.php', {
-            Cargo:              $scope.txtTituloOferta,
-            Publicador:         $scope.txtPublicador,
-            Categoria:          $scope.txtCategoria,
-            Renta:              $scope.txtRenta,
-            TipoContrato:       $scope.txtTipoContrato,
-            EtapasEvaluacion:   $scope.txtEtapasEvaluacion,
-            Modalidad:          $scope.txtModalidad,
-            DescripcionGeneral: $scope.txtDescripcionGeneral,
-            BeneficiosVentajas: $scope.txtBeneficiosVentajas,
-            Requisitos:         $scope.txtRequisitos,
-            Imagen:             $scope.myCroppedImage,
-            FechaCaduca:        $scope.txtFechaCaduca,
-            CorreoPublicador:   $scope.txtCorreoPublicador
+        $http.post('http://localhost:3000/ofertas/', {
+            cargo:              $scope.txtTituloOferta,
+            publicador:         $scope.txtPublicador,
+            categoria:          $scope.txtCategoria,
+            sueldo:             $scope.txtRenta,
+            tipocontrato:       $scope.txtTipoContrato,
+            descripciongeneral: $scope.txtDescripcionGeneral,
+            beneficiosventajas: $scope.txtBeneficiosVentajas,
+            requisitos:         $scope.txtRequisitos,
+            imagen:             $scope.myCroppedImage,
+            fechatermino:       $scope.txtFechaCaduca,
+            correoPublicador:   $scope.txtCorreoPublicador,
+			pais:				$scope.Pais,
+			fechacreacion:		date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2),
+			estado: true
+
         })
         .success(function(data) {
         		console.log(data);
