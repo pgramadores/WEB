eeb69a4fe3a747dd8495e656ade1586286d07849
Agustin Gonzalez <Agustin Gonzalez>
2016-11-27 13:10:30 -0300

    Finalizado creador de perfiles

--- js/Controller/ControllerCreaPerfil.js
+++ js/Controller/ControllerCreaPerfil.js
@@ -1,15 +1,15 @@
 angularRoutingApp.controller('CreaPerfilController', function($scope,$rootScope,$http,env,md5,ngNotify) {
 
 
-    $scope.perfil = [];
+    $scope.perfil = {};
     $scope.myImage='';
-    $scope.perfil.foto='';
+    $scope.myCroppedImage='';
     var handleFileSelect=function(evt) {
         var file=evt.currentTarget.files[0];
         var reader = new FileReader();
         reader.onload = function (evt) {
             $scope.$apply(function($scope){
-                $scope.perfil.foto = evt.target.result;
+                $scope.myImage=evt.target.result;
             });
         };
         reader.readAsDataURL(file);
@@ -21,26 +21,26 @@ angularRoutingApp.controller('CreaPerfilController', function($scope,$rootScope,
         $scope.Paises = data;        
     });
 
+
     $scope.registrar = function (){
         if ($scope.perfil.contrasena == $scope.reContrasena){
-            $scope.perfil.contrasena = md5.createHash($scope.perfil.contrasena);
-            $http.post(env.APIREST+"/perfil/",$scope.perfil)        
-            .success(function(data) {
-                ngNotify.addTheme('pro-gramadores', 'pro-gramadores');
-                ngNotify.set('<i class="fa fa-check"></i> Registrado exitosamente, a la brevedad recibirás un correo electrónico', {
-                    position: 'top',
-                    sticky: false,
-                    type: 'success',
-                    duration: 5000,
-                    theme: 'pro-gramadores',
-                    html: true,
-                    button: true
-                });
-                console.log(data);
+             $scope.perfil.contrasena = md5.createHash($scope.perfil.contrasena);
+
+            if($scope.myCroppedImage != "")
+                $scope.perfil.foto = $scope.myCroppedImage;
+                $http.post(env.APIREST+"/perfil/", $scope.perfil)        
+                .success(function(data){
+                    console.log(data);
+                    if(data != null){
+                        alert("Registrado exitosamente");
+                        window.location = env.DOMINIO;
+                    }else{
+                        alert("El correo ya se encuentra asociado con pro-ramadores");
+                    }
             })
                 .error(function(data) {
                 console.log('Error: ' + data);
             });
-         }
+          }
     }
 });
