16e2932bc908f8aa136df56b764cdfcf7f325309
driquelmemurua <driquelmemurua@gmail.com>
2016-11-27 11:19:58 -0300

    index: se agrego funcionalidad a la barra de busqueda (perfiles), controller:manda datos con scope a detallePerfil.html

--- js/Controller/ControllerDetallePerfil.js
+++ js/Controller/ControllerDetallePerfil.js
@@ -1,63 +1,25 @@
-angularRoutingApp.controller('DetallePerfilController', function($scope,$rootScope,$routeParams,$location) {
-    $routeParams.correo;
-    var data = {
-        nombrecompleto    : 'Agustin Gonzalez Murua',
-        correo          : 'agustin.gonzalez@pro-gramadores.cl',
-        nacionalidad    : 'CL',
-        foto            : null,
-        redessociales   : [
-            {
-                "icono"   : "fa-twitter",
-                "nombre"  : "Twitter",
-                "link"    : "www.twitter.com"
-            },
-            {
-                "icono"   : "fa-facebook",
-                "nombre"  : "Facebook",
-                "link"    : "www.facebook.com"
-            }
-        ],
-        experiencias    : [
-            {
-                nombreempresa   : "GL Group",
-                cargo           : "Algo especial",
-                desde           : new Date(2016,7,26,8,30,0,0),
-                hasta           : "Actualidad",
-                descripcion     : "Hice muchas cosas"
-            }
-        ],
-        bio             : "Mi biografía",
-        aptitudes       : [
-            {
-                tipo        : "Workshop",
-                nombre      : "Event Loop Hackaton",
-                habilidades   : [
-                    {
-                        nombre : "JavaScript"
-                    }
-                ],
-                fechaasistencia : new Date(2016,11,26,9,30,0,0)
-            }
-        ]
-    };
+angularRoutingApp.controller('DetallePerfilController', function($scope,$rootScope,$routeParams,$location, env) {
 
-    $rootScope.Titulo = "Perfil de " + data.nombres;
-    $scope.Ubicacion = escape($location.absUrl());
-    $scope.CompartirFacebook = function () {
-        console.log($location.absUrl());
-        FB.ui({
-            method: 'feed',
-            name: data.cargo,
-            link: $location.absUrl(),
-            caption: 'Pro-Gramadores.org :: El poder del código',
-            description : "Perfil de " + data.nombres,
-            picture : 'https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAlYAAAAJDQ0MWNjZWFiLTQwZTYtNDJiZC1hNWY3LTM3MWE5OGM1Mzc5MQ.png',
-        }, function(response){
+    $http.get(env.APIREST+'/perfil/'+$routeParams.correo)
+    .success(function(data) {
+        $rootScope.Titulo = "Perfil de " + data.nombres;
+        $scope.Ubicacion = escape($location.absUrl());
+        $scope.CompartirFacebook = function () {
+            console.log($location.absUrl());
+            FB.ui({
+                method: 'feed',
+                name: data.cargo,
+                link: $location.absUrl(),
+                caption: 'Pro-Gramadores.org :: El poder del código',
+                description : "Perfil de " + data.nombres,
+                picture : 'https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAlYAAAAJDQ0MWNjZWFiLTQwZTYtNDJiZC1hNWY3LTM3MWE5OGM1Mzc5MQ.png',
+            }, function(response){
 
-        });
-    };
-    $scope.Perfil = data; 
-    $scope.Redessociales = data.redessociales;
-    $scope.Experiencias= data.experiencias;
-    $scope.Aptitudes = data.aptitudes;
+            });
+        };
+        $scope.Perfil = data; 
+        $scope.Redessociales = data.redessociales;
+        $scope.Experiencias= data.experiencias;
+        $scope.Aptitudes = data.aptitudes;
+    });
 });
